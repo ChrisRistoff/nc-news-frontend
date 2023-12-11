8e78b5cc4d0a50d82f1087b141afb5f73b601be0
Krasyo <doanusho@gmail.com>
2023-12-11 15:16:32 +0000

    swap links for https on redner

--- src/utils/getComments.jsx
+++ src/utils/getComments.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getComments = async (articleId) => {
 
   try {
-    let url = `http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com/api/articles/${articleId}/comments`;
+    let url = `https://nc-news-api-62ip.onrender.com/api/articles/${articleId}/comments`;
     const comments = await axios.get(url);
 
     return comments.data.comments;
