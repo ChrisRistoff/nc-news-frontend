14982a0af1fab3bdd2a5cd4902e2f02c55021d2b
Krasyo <doanusho@gmail.com>
2024-01-23 07:26:44 +0000

    Change api link

--- src/utils/axiosCrete.js
+++ src/utils/axiosCrete.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com/api
 export const api = axios.create({
-  baseURL: "http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com/api",
+  baseURL: "https://krasen.site/api",
   headers: {
     "Content-Type": "application/json"
   }
