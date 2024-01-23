9a16ec930557b579d5bfbb29be6bcb67b2433fde
Krasyo <doanusho@gmail.com>
2024-01-23 07:23:25 +0000

    test new pipeline

--- scripts/deploy_script_fe.sh
+++ scripts/deploy_script_fe.sh
@@ -3,22 +3,18 @@
 set -e
 
 CONTAINER_NAME="nc_news_c_fe"
-IMAGE_NAME="nc_news_fe"
+IMAGE_NAME="krahristov/news-frontend:latest"
 APP_DIR="$HOME/nc-news-frontend"
 
 echo "Starting deployment..."
 
-cd "$APP_DIR"
 
-echo "Pulling latest changes from Git..."
-git pull origin main
+echo "Pulling the Docker image..."
+sudo docker pull $IMAGE_NAME
 
 echo "Stopping the existing Docker container..."
 sudo docker stop $CONTAINER_NAME || true
 sudo docker rm $CONTAINER_NAME || true
-
-echo "Building the new Docker image..."
-sudo docker build -t $IMAGE_NAME .
 
 echo "Running the new Docker container..."
 sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
